ff53b3398c2facaa01eea8e85f9817ac7bb5830b
zhanghj31 <zhanghj31@lenovo.com>
2025-04-14 17:47:55 +0800

    feature/微调

--- emsp/src/test/java/com/volvo/emsp/AccountServiceTest.java
+++ emsp/src/test/java/com/volvo/emsp/AccountServiceTest.java
@@ -1,10 +1,20 @@
 package com.volvo.emsp;
 
+import com.volvo.emsp.domain.Account;
+import com.volvo.emsp.reposity.AccountRepository;
+import com.volvo.emsp.request.AccountRequest;
+import com.volvo.emsp.response.AccountResponse;
+import com.volvo.emsp.service.AccountService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
