80a0919dac2f2c8fe62ef8e89e55fe64dcc06042
zhanghj31 <zhanghj31@lenovo.com>
2025-04-16 09:31:39 +0800

    feature/临时去掉swagger

--- emsp/src/main/java/com/volvo/emsp/EmspApplication.java
+++ emsp/src/main/java/com/volvo/emsp/EmspApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmspApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EmspApplication.class, args);
     }
-
 }
